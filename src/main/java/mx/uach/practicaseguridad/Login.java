d44d1eced2afea848d4eda547822183938006f64
Pedro Barriga <Pedro Barriga>
2016-06-26 10:52:30 -0700

    correccion en validaciones de campos vacios en servidor

--- src/main/java/mx/uach/practicaseguridad/Login.java
+++ src/main/java/mx/uach/practicaseguridad/Login.java
@@ -22,10 +22,9 @@ import spark.template.freemarker.FreeMarkerEngine;
 public class Login {
 
     private static final Logger LOGGER = LogManager.getLogger("Login");
-    
-    
+
     public static void main(String[] args) {
-       
+
         /**
          * Ruta inicial de la app muestra el formulario de registro de la app.
          */
@@ -34,11 +33,11 @@ public class Login {
             String validador = "";
             String nombre = "";
             String mensaje = "";
-            
+
             attributes.put("validador", validador);
             attributes.put("nombre", nombre);
             attributes.put("mensaje", mensaje);
-            
+
             return new ModelAndView(attributes, "registrar.ftl");
         }, new FreeMarkerEngine());
 
@@ -47,53 +46,53 @@ public class Login {
          */
         post("/registrar", (req, res) -> {
             Map<String, Object> attributes = new HashMap<>();
-            
+
             String nombre = req.queryParams("nombre");
             String email = req.queryParams("email");
             String password = req.queryParams("password");
             String apellidos = req.queryParams("apellidos");
             String validador = "";
             String mensaje = "";
-            
-            if(nombre.equals("")){
+
+            if (nombre.equals("")) {
                 //Falta el usuario
                 validador = "Por favor complete el campo nombre";
-                attributes.put("nombre",nombre);
-            }else if (apellidos.equals("")){
+                attributes.put("nombre", nombre);
+            } else if (apellidos.equals("")) {
                 validador = "Por favor complete el campo apellidos";
-            }else if (email.equals("")){
+            } else if (email.equals("")) {
                 validador = "Por favor complete el campo email";
-            }else if (password.equals("")){
+            } else if (password.equals("")) {
                 validador = "Por favor complete el campo password";
+            } else {
+
+                try {
+
+                    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PracticaSeguridadPU");
+
+                    EntityManager em = emf.createEntityManager();
+
+                    Usuario usuario = new Usuario(email, nombre, apellidos, password);
+                    em.getTransaction().begin();
+                    em.persist(usuario);
+                    em.getTransaction().commit();
+                    em.close();
+                    mensaje = "Registro ha sido exitoso";
+
+                } catch (Exception ex) {
+                    LOGGER.error(String.format("El usuario %s no se ha podido registrar. %s", email, ex.getMessage()));
+                    validador = "No se pudo registrar el usuario";
+
+                }
             }
-            
-            
-            try{
-                
-                EntityManagerFactory emf = Persistence.createEntityManagerFactory("PracticaSeguridadPU");
-                
-                EntityManager em = emf.createEntityManager();
-                
-                Usuario usuario = new Usuario(email,nombre,apellidos,password);             
-                em.getTransaction().begin();                
-                em.persist(usuario);                
-                em.getTransaction().commit();             
-                em.close();
-                mensaje="Registro ha sido exitoso";
-                        
-            }catch(Exception ex){
-                LOGGER.error(String.format("El usuario %s no se ha podido registrar. %s",email,ex.getMessage()));
-                validador = "No se pudo registrar el usuario";
-                
-            }
-               
+
             attributes.put("validador", validador);
             attributes.put("nombre", "");
             attributes.put("mensaje", mensaje);
             //TODO Guardar en Base de datos.
             return new ModelAndView(attributes, "registrar.ftl");
         }, new FreeMarkerEngine());
-        
+
         /**
          * Ruta para verificar el inicio de sesión.
          */
@@ -101,48 +100,47 @@ public class Login {
             Map<String, Object> attributes = new HashMap<>();
             String email = req.queryParams("email");
             String password = req.queryParams("password");
-            
+
             String validador = "";
             String nombre = "";
             String mensaje = "";
-            
+
             String usuario = "n/a";
-            
+
             attributes.put("validador", validador);
             attributes.put("nombre", nombre);
-            
-            try{
+
+            try {
                 EntityManagerFactory emf = Persistence.createEntityManagerFactory("PracticaSeguridadPU");
-                
+
                 EntityManager em = emf.createEntityManager();
-                
+
                 Query q = em.createQuery("SELECT u from Usuario u WHERE u.email=:arg1");
                 q.setParameter("arg1", email);
-                
+
                 Usuario usuarioToValidate = (Usuario) q.getSingleResult();
-                
-                if(usuarioToValidate.getPassword().equals(usuarioToValidate.hashPassword(password))){
-                LOGGER.trace(String.format("El usuario %s ha iniciado sesión.", email));
-                usuario = email;
-                attributes.put("mensaje", mensaje);
-                attributes.put("usuario", usuario);
-                return new ModelAndView(attributes, "home.ftl");
-            }else{
-                mensaje = "Usuario y/o Password son incorrectos.";
-                LOGGER.error(String.format("password en BD %s y el mandado es %s - %s",usuarioToValidate.getPassword(),password,usuarioToValidate.hashPassword(password)));
 
-            }
-                
+                if (usuarioToValidate.getPassword().equals(usuarioToValidate.hashPassword(password))) {
+                    LOGGER.trace(String.format("El usuario %s ha iniciado sesión.", email));
+                    usuario = email;
+                    attributes.put("mensaje", mensaje);
+                    attributes.put("usuario", usuario);
+                    return new ModelAndView(attributes, "home.ftl");
+                } else {
+                    mensaje = "Usuario y/o Password son incorrectos.";
+                    LOGGER.error(String.format("password en BD %s y el mandado es %s - %s", usuarioToValidate.getPassword(), password, usuarioToValidate.hashPassword(password)));
+
+                }
+
                 em.close();
-            }catch(Exception ex){
-             mensaje = "Usuario y/o Password son incorrectos.";         
-           LOGGER.error(String.format("%s",ex.getMessage()));
+            } catch (Exception ex) {
+                mensaje = "Usuario y/o Password son incorrectos.";
+                LOGGER.error(String.format("%s", ex.getMessage()));
 
             }
-            
-            
+
             attributes.put("mensaje", mensaje);
-            
+
             return new ModelAndView(attributes, "registrar.ftl");
         }, new FreeMarkerEngine());
     }
